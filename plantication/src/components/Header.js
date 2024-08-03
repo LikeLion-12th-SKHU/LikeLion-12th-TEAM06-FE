0d728cee0f866b4448f2ccb662644874a237e0bb
rryuuni <fkdbs1122@naver.com>
2024-08-03 21:34:41 +0900

    Feat: 푸터, 헤더

---
+++ plantication/src/components/Header.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <MainContainer>
+      <HeaderSt>
+        <Logo
+          src="/assets/img/logo.png"
+          alt="로고"
+          onClick={() => navigate("/")}
+        />
+        <BtnStyle onClick={() => navigate("/auth")}>로그인</BtnStyle>
+      </HeaderSt>
+    </MainContainer>
+  );
+}
+
+export default Header;
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 390px;
+  box-sizing: border-box;
+  background-color: #fdfdfd;
+  margin: 0 auto;
+`;
+
+const HeaderSt = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
+`;
+
+const BtnStyle = styled.button`
+  margin-top: 5px;
+  border: none;
+  background-color: #025d00;
+  border-radius: 5px;
+  color: #fff;
+  height: 25px;
+  width: 60px;
+  cursor: pointer;
+`;
+
+const Logo = styled.img`
+  width: auto;
+`;
